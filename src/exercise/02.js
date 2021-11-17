c7f0563f839f04492340f17c9c5791ef7491acad
Darren Seet <darrens@rev.io>
2021-11-17 07:43:12 -0500

    feat: useEffect
    Adding usage of useEffect hook to perform operations with side effects. Example setting local storage

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,16 +1,21 @@
 // useEffect: persistent state
 // http://localhost:3000/isolated/exercise/02.js
 
-import * as React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+  const [name, setName] = useState(
+    window.localStorage.getItem('name') || initialName
+  )
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
+  useEffect(() => {
+    window.localStorage.setItem('name', name)
+  })
 
   function handleChange(event) {
     setName(event.target.value)
@@ -19,7 +24,7 @@ function Greeting({initialName = ''}) {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" autoComplete="off" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
