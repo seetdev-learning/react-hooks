240042d51a7e417f53ca4c468ab9d651df8b6ec6
Darren Seet <darrens@rev.io>
2021-11-17 09:06:34 -0500

    feat: Custom hooks Moving useState and useEffect into a custom hook that allows React state and local storage synching in a hook

---
+++ src/exercise/02.extra-3.js
@@ -0,0 +1,36 @@
+// useEffect: persistent state
+// http://localhost:3000/isolated/exercise/02.js
+
+import React, {useEffect, useState} from 'react'
+
+function useLocalStorageState(key, initialState) {
+  const [state, setState] = useState(() => window.localStorage.getItem(key) || initialState)
+  useEffect(() => {
+    window.localStorage.setItem(key, state)
+  }, [key, state])
+  return [state, setState] 
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName)
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" autoComplete="off" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting />
+}
+
+export default App
