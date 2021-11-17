ea499825f29798f47e44c023178e1a0aa6c4d248
Darren Seet <darrens@rev.io>
2021-11-17 09:09:10 -0500

    feat: Make custom hook generic and add useRef hook Add default value type checking to allow functions that mimic the memo functionality offered like the React useState hook. Introduce useRef hook that allows the key to be memorized so that changing of key name can be handled

---
+++ src/exercise/02.extra-4.js
@@ -0,0 +1,51 @@
+// useEffect: persistent state
+// http://localhost:3000/isolated/exercise/02.js
+
+import React, {useEffect, useRef, useState} from 'react'
+
+function useLocalStorageState(key, 
+  defaultValue = '', 
+  { serialized = JSON.stringify, deserialized = JSON.parse}) {
+  
+    const [state, setState] = useState(() => {
+    const localStorageValue = window.localStorage.getItem(key)
+    if(localStorageValue) {
+      return deserialized(localStorageValue)
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+
+  const prevKeyRef = useRef(key)
+  useEffect(() => {
+    const prevKey = prevKeyRef.current
+    if(prevKey !== key) window.localStorage.removeItem(prevKey)
+    
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialized(state))
+  }, [key, state, serialized])
+  
+  return [state, setState] 
+}
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName)
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" autoComplete="off" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting />
+}
+
+export default App
