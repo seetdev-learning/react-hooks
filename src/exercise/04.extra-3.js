8b074ea84bab6eedd7230b21698174f7e8047cc7
Darren Seet <darrens@rev.io>
2021-11-18 22:26:39 -0500

    feat: Complex derived state with Undo functionaliaty

---
+++ src/exercise/04.extra-3.js
@@ -0,0 +1,147 @@
+// useState: tic tac toe
+// http://localhost:3000/isolated/exercise/04.js
+
+import React, {useEffect, useRef, useState} from 'react'
+
+function useLocalStorageState(
+  key, 
+  defaultValue='', 
+  { serializer = JSON.stringify, deserializer = JSON.parse} = {}
+) {
+  const [state, setState] = useState(
+    () => {
+      const localStorageValue = window.localStorage.getItem(key)
+      if(localStorageValue) return deserializer(localStorageValue)
+      if(typeof defaultValue === 'function') return defaultValue()
+      return defaultValue
+    }
+  )
+  
+  const keyRef = useRef(key)
+  useEffect(() => {
+    if(keyRef.current !== key) {
+      window.localStorage.removeItem(keyRef.current)
+    }
+    window.localStorage.setItem(key, serializer(state))
+  }, [key, state, serializer])
+
+  return [state, setState]
+}
+
+function Board({squares, onClick}) {
+  function renderSquare(i) {
+    return (
+      <button className="square" onClick={() => onClick(i)}>
+        {squares[i]}
+      </button>
+    )
+  }
+
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  )
+}
+
+function Game() {
+  const [history, setHistory] = useLocalStorageState('tictacktoe.history', [Array(9).fill(null)]) 
+  const [currentStep, setCurrentStep] = useLocalStorageState('tictacktoe.step', 0)
+  const currentSquares = history[currentStep]
+  const nextValue = calculateNextValue(currentSquares)
+  const winner = calculateWinner(currentSquares)  
+  const status = calculateStatus(winner, currentSquares, nextValue)
+  const moves = history.map((_, step) => {
+    const description = step === 0 ? "Go to game start" : `Go to move #${step}` 
+    const isCurrentStep = step === currentStep
+    return (
+      <li key={step}>
+        <button onClick={() => setCurrentStep(step)} disabled={isCurrentStep}>{description} {isCurrentStep ? '(current)' : null}</button>
+      </li>
+    )
+  })
+
+  function selectSquare(square) {
+    if(winner || currentSquares[square]) return;
+    const newHistory = history.slice(0, currentStep + 1)
+    const newSquares = [...currentSquares]
+    newSquares[square] = nextValue
+    setHistory([...newHistory, newSquares])
+    setCurrentStep((step) => step + 1)
+  }
+
+  function restart() {
+    setHistory([Array(9).fill(null)])
+    setCurrentStep(0)
+  }  
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board onClick={selectSquare} squares={currentSquares} />
+        <button className="restart" onClick={restart}>
+          restart
+        </button>        
+      </div>
+      <div className="game-info">
+        <div className="status">{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+}
+
+// eslint-disable-next-line no-unused-vars
+function calculateStatus(winner, squares, nextValue) {
+  return winner
+    ? `Winner: ${winner}`
+    : squares.every(Boolean)
+    ? `Scratch: Cat's game`
+    : `Next player: ${nextValue}`
+}
+
+// eslint-disable-next-line no-unused-vars
+function calculateNextValue(squares) {
+  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O'
+}
+
+// eslint-disable-next-line no-unused-vars
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
+function App() {
+  return <Game />
+}
+
+export default App
