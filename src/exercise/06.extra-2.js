cb13c3d58018cadf0bdbc45cd6a0bc039f9cbf6a
Darren Seet <darrens@rev.io>
2021-11-19 20:41:59 -0500

    feat: Using a status flag Use a status state by setting it to these string values rather than relying on existing state or booleans. Learn more about this concept here: https://kentcdodds.com/blog/stop-using-isloading-booleans

---
+++ src/exercise/06.extra-2.js
@@ -0,0 +1,61 @@
+// useEffect: HTTP requests
+// http://localhost:3000/isolated/exercise/06.js
+
+import * as React from 'react'
+import {fetchPokemon, PokemonForm, PokemonInfoFallback, PokemonDataView} from '../pokemon'
+
+function PokemonInfo({pokemonName}) {
+  const [status, setStatus] = React.useState('idle')
+  const [pokemon, setPokemon] = React.useState(null)
+  const [error, setError] = React.useState(null)
+  React.useEffect(() => {
+    if(!pokemonName) return
+    setStatus('pending')
+    fetchPokemon(pokemonName).then(
+      pokemonData => {
+        setPokemon(pokemonData)
+        setStatus('resolved')
+      },
+      error => {
+        setError(error)
+        setStatus('rejected')
+      }
+    )
+  }, [pokemonName])
+
+  if(status === 'idle') {
+    return 'Submit a pokemon'
+  }
+
+  if(status === 'rejected') {
+    return <div role="alert">
+      There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+    </div>
+  }
+
+  if(status === 'pending')  {
+    return <PokemonInfoFallback name={pokemonName} />
+  }
+
+  return <PokemonDataView pokemon={pokemon} />
+}
+
+function App() {
+  const [pokemonName, setPokemonName] = React.useState('')
+
+  function handleSubmit(newPokemonName) {
+    setPokemonName(newPokemonName)
+  }
+
+  return (
+    <div className="pokemon-info-app">
+      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+      <hr />
+      <div className="pokemon-info">
+        <PokemonInfo pokemonName={pokemonName} />
+      </div>
+    </div>
+  )
+}
+
+export default App
